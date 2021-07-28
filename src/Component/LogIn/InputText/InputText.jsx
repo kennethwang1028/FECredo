c9b44dc531ed784a5698b48a34c124620d8d75e2
kenneth wang <kennethwang0427@gmail.com>
2021-07-28 13:00:36 -0700

    inputext test

--- src/Component/LogIn/InputText/InputText.jsx
+++ src/Component/LogIn/InputText/InputText.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import {
   SignInInput,
@@ -14,17 +15,34 @@ const InputText = (props) => {
     <div>
       <div>{text}</div>
       <SignInInput
+        data-testid="input"
         warning={isWarning}
         type="text"
         onChange={handleChange}
       />
       {isWarning ? (
-        <WarningText>
+        <WarningText
+          data-testid="warning"
+        >
           { `!! ${warningText} !!` }
         </WarningText>
       ) : null}
     </div>
   );
+};
+
+InputText.propTypes = {
+  text: PropTypes.string,
+  isWarning: PropTypes.bool,
+  handleChange: PropTypes.func,
+  warningText: PropTypes.string,
+};
+
+InputText.defaultProps = {
+  text: '',
+  isWarning: false,
+  handleChange: () => 1,
+  warningText: '',
 };
 
 export default InputText;
---
+++ src/Component/LogIn/InputText/InputText.test.js
@@ -0,0 +1,69 @@
+/**
+ * @jest-environment jsdom
+ */
+import React from 'react';
+import {
+  render,
+  cleanup,
+  fireEvent,
+} from '@testing-library/react';
+import '@testing-library/jest-dom';
+
+import InputText from './InputText';
+
+afterEach(cleanup);
+
+const test1 = 'hi';
+const test2 = true;
+const test3 = false;
+const test4 = jest.fn();
+const test5 = 'hello world';
+
+describe('render text correct', () => {
+  it('render warning text correct', () => {
+    const { queryByTestId } = render(
+      <InputText
+        text={test1}
+        isWarning={test2}
+        handleChange={test4}
+        warningText={test5}
+      />,
+    );
+    expect(queryByTestId('warning').textContent).toBe(`!! ${test5} !!`);
+  });
+  it('not render warning text correct', () => {
+    const { queryByTestId } = render(
+      <InputText
+        text={test1}
+        isWarning={test3}
+        handleChange={test4}
+        warningText={test5}
+      />,
+    );
+    expect(queryByTestId('warning')).toBeNull();
+  });
+});
+
+describe('function works correct', () => {
+  it('input change invoke test4', () => {
+    const { queryByTestId } = render(
+      <InputText
+        text={test1}
+        isWarning={test2}
+        handleChange={test4}
+        warningText={test5}
+      />,
+    );
+    fireEvent.change(queryByTestId('input'), { target: { value: 'ab' } });
+    expect(test4).toHaveBeenCalledTimes(1);
+    fireEvent.change(queryByTestId('input'), { target: { value: 'abc' } });
+    expect(test4).toHaveBeenCalledTimes(2);
+  });
+});
+
+it('should have default addUserReview and moreReview', () => {
+  expect(InputText.defaultProps.text).toBeDefined();
+  expect(InputText.defaultProps.isWarning).toBeDefined();
+  expect(InputText.defaultProps.handleChange()).toBeDefined();
+  expect(InputText.defaultProps.warningText).toBeDefined();
+});
